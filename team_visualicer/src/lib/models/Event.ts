d77dbfc2b19ff55895d117d28593bd81738d82ce
Javier Rodriguez <rodriguez.garcia.javier.602@gmail.com>
2025-12-08 16:50:02 -0600

    Final Esperado 1

--- team_visualicer/src/lib/models/Event.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema, models } from "mongoose";
-
-const EventSchema = new Schema({
-  title: { type: String, required: true },
-  date: { type: Date, required: true },
-  allDay: { type: Boolean, default: false },
-}, { timestamps: true });
-
-export default models.Event || mongoose.model("Event", EventSchema);
